dc552575e1d609d8e36004f4c47d2cfd3356c131
SarahCClausen <43234263+SarahCClausen@users.noreply.github.com>
2019-05-29 13:32:49 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
-var myIndex = 0;
-carousel();
+var myIndex = 0; // start med at nulstille myIndex. myIndex bruges til at tælle og holde styr på hvilket billede vi er nået til.
+
+carousel(); // Kald funktionen og start showet :D
 
 function carousel() {
-  var i;
-  var x = document.getElementsByClassName("foto"); // Take the foto from foto
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";  
+  var fotos = document.querySelector(".foto"); // Find i DOM træet alle elementer med class "foto"
+  // loop alle fotos igennem
+  for (var i = 0; i < fotos.length; i++) { // for hvert af fotos
+    fotos[i].style.display = "none";  // sæt display til "none", som dermed fjerner billedet fra renderingen
   }
-  myIndex++;
-  if (myIndex > x.length) {myIndex = 1}    
-  x[myIndex-1].style.display = "unset";  
-  setTimeout(carousel, 2500); // 0Change image every 2 seconds
+  if (myIndex > fotos.length) {myIndex = 0} // hvis vores tæller er størrere end antallet af fotos, sæt tæller til 1   
+  fotos[myIndex].style.display = "unset"; // fjern display:none fra det billede vi vil vise  
+  myIndex++; // læg een til vores tæller
+  setTimeout(carousel, 2500); // Gentag denne funktion hver 2.5 sekund
 }
+Collapse
